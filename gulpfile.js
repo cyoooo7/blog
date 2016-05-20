087d840564f03aac72765913356fe2114cd8d1c0
程遥 <chengyao@cvte.com>
2016-05-20 22:09:02 +0800

    添加自动更新脚本。

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+var gulp = require('gulp');
+var exec = require('child_process').exec;
+
+var paths = {
+  build: 'build/'
+};
+
+function update() {
+  console.log('git pull >>>');
+  exec('git pull', function(a) {
+    console.log('git pull >>> ' + a);
+    console.log('wintersmith build >>> ');
+    exec('wintersmith build', function(b) {
+      console.log('wintersmith build >>> ' + b);
+      console.log('>>> updated.');
+      setTimeout(function() {
+        update();
+      }, 1 * 60 * 1000);
+    });
+  });
+}
+
+gulp.task('watch', function() {
+  exec('static-server -p 8001', {
+    cwd: paths.build
+  });
+  update();
+});
+
+gulp.task('default', ['watch']);
